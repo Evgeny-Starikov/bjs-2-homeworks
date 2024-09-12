76ec0e3214dd008ce80b34f80d61c42eada7c5bf
Evgeny Starikov <genestar@yandex.ru>
2024-09-12 13:34:33 +0300

    task.js moddified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,18 @@
-
 "use strict";
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = b ** 2 -4 * a * c;
+	let d = b ** 2 - 4 * a * c;
 	if (d > 0) {
 		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
 		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-		
+
 	} else if (d === 0) {
 		arr[0] = -b / (2 * a);
-		
+
 	} else {
 		arr = [];
-			}
+	}
 	return arr;
 }
 
@@ -21,17 +20,17 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  let per = parseInt(percent) / 100 / 12; //процентная ставка в месяц
-  let con = parseInt(contribution); 	  //начальный взнос	
-  let am = parseInt(amount);  			  //общая стоимость	
-  let count = parseInt(countMonths); //месяц окончания
+	let per = parseInt(percent) / 100 / 12; //процентная ставка в месяц
+	let con = parseInt(contribution); //начальный взнос	
+	let am = parseInt(amount); //общая стоимость	
+	let count = parseInt(countMonths); //месяц окончания
 
-  let loanBody = amt - contr; //тело кредита
+	let loanBody = amt - contr; //тело кредита
 
-  let payment = loanBody * (perCent + (perCent / (((1 + perCent) ** count) - 1))); //Ежемесячная оплата 
-  
-  let totalAmount = (payment * count).toFixed(2);
-    
-  console.log(totalAmount);
-  return +(totalAmount);
+	let payment = loanBody * (perCent + (perCent / (((1 + perCent) ** count) - 1))); //Ежемесячная оплата 
+
+	let totalAmount = (payment * count).toFixed(2);
+
+	console.log(totalAmount);
+	return +(totalAmount);
 }
