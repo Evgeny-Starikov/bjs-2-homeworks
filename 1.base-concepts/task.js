18ac1ba12a115ed9d915462c094e1126e6ac3084
Evgeny Starikov <genestar@yandex.ru>
2024-09-12 13:11:17 +0300

    Tasks 1, 2 - are completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 -4 * a * c;
+	if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+		
+	} else if (d === 0) {
+		arr[0] = -b / (2 * a);
+		
+	} else {
+		arr = [];
+			}
+	return arr;
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  let per = parseInt(percent) / 100 / 12; //процентная ставка в месяц
+  let con = parseInt(contribution); 	  //начальный взнос	
+  let am = parseInt(amount);  			  //общая стоимость	
+  let count = parseInt(countMonths); //месяц окончания
+
+  let loanBody = amt - contr; //тело кредита
+
+  let payment = loanBody * (perCent + (perCent / (((1 + perCent) ** count) - 1))); //Ежемесячная оплата 
   
+  let totalAmount = (payment * count).toFixed(2);
+    
+  console.log(totalAmount);
+  return +(totalAmount);
 }
