ac4779d46043c1f91452efb0ed034e46c4e006e3
Evgeny Starikov <165017803+Evgeny-Starikov@users.noreply.github.com>
2024-09-16 13:47:40 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,27 @@
 function getArrayParams(...arr) {
-  if (arr.length === 0) {
-    return 0;
-  }
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  let avg = 0;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-    sum = sum + arr[i];
-    avg = sum / arr.length;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+	let avg = 0;
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		sum = sum + arr[i];
+		avg = sum / arr.length;
+	}
 
-  return {
-    min: min,
-    max: max,
-    avg: +(avg.toFixed(2))
-  }
+	return {
+		min: min,
+		max: max,
+		avg: +(avg.toFixed(2))
+	}
 }
 
 getArrayParams(10, 10, 11, 20, 10);
@@ -29,18 +29,18 @@ getArrayParams(10, 10, 11, 20, 10);
 
 
 function summElementsWorker(...arr) {
-  if (arr.length === 0) {
-    return 0;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
 
-  let sum = 0;
+	let sum = 0;
 
-  for (i = 0; i < arr.length; i++) {
+	for (i = 0; i < arr.length; i++) {
 
-    sum += arr[i];
-  }
+		sum += arr[i];
+	}
 
-  return sum;
+	return sum;
 
 }
 
@@ -50,19 +50,19 @@ summElementsWorker(10, 10, 11, 20, 10);
 
 function differenceMaxMinWorker(...arr) {
 
-  if (arr.length === 0) {
-    return 0;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
 
-  let max = Math.max(...arr);
-  let min = Math.min(...arr);
-  let difference = max - min;
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
+	let difference = max - min;
 
-  if (difference === -Infinity) {
-    return 0;
-  } else {
-    return difference;
-  }
+	if (difference === -Infinity) {
+		return 0;
+	} else {
+		return difference;
+	}
 }
 
 differenceMaxMinWorker(10, 5, 16, 13, 25);
@@ -70,23 +70,23 @@ differenceMaxMinWorker(10, 5, 16, 13, 25);
 
 
 function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0) {
-    return 0;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
 
-  sumEvenElement = 0;
-  sumOddElement = 0;
+	sumEvenElement = 0;
+	sumOddElement = 0;
 
-  for (i = 0; i < arr.length; i++) {
+	for (i = 0; i < arr.length; i++) {
 
-    if (arr[i] % 2 == 0) {
-      sumEvenElement += arr[i];
-    } else {
-      sumOddElement += arr[i];
-    }
-  }
+		if (arr[i] % 2 == 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
 
-  return sumEvenElement - sumOddElement;
+	return sumEvenElement - sumOddElement;
 
 }
 
@@ -95,47 +95,47 @@ differenceEvenOddWorker(61, 206, 328, 284);
 
 function averageEvenElementsWorker(...arr) {
 
-  if (arr.length === 0) {
-    return 0;
-  }
+	if (arr.length === 0) {
+		return 0;
+	}
 
-  sumEvenElement = 0;
-  countEvenElement = 0;
+	sumEvenElement = 0;
+	countEvenElement = 0;
 
-  for (i = 0; i < arr.length; i++) {
+	for (i = 0; i < arr.length; i++) {
 
-    if (arr[i] % 2 == 0) {
-      sumEvenElement += arr[i];
-      countEvenElement++;
-    }
-  }
+		if (arr[i] % 2 == 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		}
+	}
 
-  return +(sumEvenElement / countEvenElement).toFixed(2);
+	return +(sumEvenElement / countEvenElement).toFixed(2);
 
 }
 
 averageEvenElementsWorker(1, 5, 6, 8);
 
 const arr = [
-  [10, 10, 11, 20, 10],
-  [67, 10, 2, 39, 88],
-  [72, 75, 51, 87, 43],
-  [30, 41, 55, 96, 62]
+	[10, 10, 11, 20, 10],
+	[67, 10, 2, 39, 88],
+	[72, 75, 51, 87, 43],
+	[30, 41, 55, 96, 62]
 ];
 
 
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
-  let maxWorkResult = -Infinity;
-  for (let i = 0; i < arrOfArr.length; i++) {
-    let max = func(...arrOfArr[i]);
-    if (maxWorkResult < max) {
-      maxWorkResult = max;
-    }
-  }
+	let maxWorkResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let max = func(...arrOfArr[i]);
+		if (maxWorkResult < max) {
+			maxWorkResult = max;
+		}
+	}
 
-  return maxWorkResult;
+	return maxWorkResult;
 
 }
 
